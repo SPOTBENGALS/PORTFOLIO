b65afaf5f9954c2064c6ff0a309a457e39def98e
jiyeong <guacamole@kakao.com>
2025-07-25 19:41:55 +0900

    프로젝트 추가

--- src/Store/Data.ts
+++ src/Store/Data.ts
@@ -2,9 +2,11 @@ import projectImg01 from "../images/001.png";
 import projectImg02 from "../images/002.png";
 import projectImg03 from "../images/003.png";
 import projectImg04 from "../images/004.png";
-import projectImg05 from "../images/005.png";
+// import projectImg05 from "../images/005.png";
 import projectImg06 from "../images/006.png";
 import projectImg07 from "../images/007.png";
+import projectImg08 from "../images/008.png";
+import projectImg09 from "../images/009.png";
 
 export const PROJECTSDATA = [
   {
@@ -32,7 +34,7 @@ export const PROJECTSDATA = [
     image: projectImg03,
     demoLink: "https://mini-games-tawny.vercel.app/",
     githubLink: "https://github.com/SPOTBENGALS/Mini-Games",
-    keyword: ["React", "Typescript", "NextJS", "SCSS", "Varcel"],
+    keyword: ["React", "Typescript", "NextJS", "SCSS", "Varcel", "Zustand"],
   },
   {
     id: 4,
@@ -44,21 +46,49 @@ export const PROJECTSDATA = [
     keyword: ["React", "Babel", "Webpack", "Module-CSS"],
   },
   {
-    id: 5,
-    projectName: "TodoList",
-    descript: "LocalStorage를 활용하여 오늘의 할일과 기본 할일 기능을 제공합니다.",
-    image: projectImg05,
-    demoLink: "https://spotbengals.github.io/TodoList/",
-    githubLink: "https://github.com/SPOTBENGALS/TodoList",
-    keyword: ["React", "Babel", "Webpack", "Typescript"],
-  },
-  {
     id: 6,
     projectName: "Cloud Business Site",
-    descript: "사내 프로젝트에서 php 기반의 HTML/CSS(Bootstrap) 파트를 담당했던 반응형 사이트입니다.",
+    descript: "php 기반의 HTML/CSS(Bootstrap) 파트를 담당했던 반응형 사이트입니다.",
     image: projectImg06,
     demoLink: "http://jiyeong.dothome.co.kr/",
     githubLink: "",
     keyword: ["HTML", "Bootstrap"],
   },
+  {
+    id: 7,
+    projectName: "LX플랫폼 GIS 분석 시스템",
+    descript: "OpenLayers 라이브러리를 활용한 GIS 분석 서비스입니다.",
+    image: projectImg07,
+    demoLink: "",
+    githubLink: "",
+    keyword: ["JSP", "Javascript"],
+  },
+  {
+    id: 8,
+    projectName: "도시침수 대응 시스템",
+    descript: "DT 플랫폼과 React-Query를 활용한 관리자 시스템, 모바일 웹앱 현장 점검 서비스입니다.",
+    image: projectImg08,
+    demoLink: "",
+    githubLink: "",
+    keyword: ["React", "Typescript", "Styled-Components", "React-Query", "Zustand", "Vercel", "Thymeleaf"],
+  },
+  {
+    id: 9,
+    projectName: "노후계획도시 주민 투표 서비스",
+    descript: "국토교통부의 투표 안건 관리 및 투표를 제공하는 서비스입니다.",
+    image: projectImg09,
+    demoLink: "",
+    githubLink: "",
+    keyword: ["React", "Typescript", "Styled-Components", "React-Query", "Zustand"],
+  },
+
+  // {
+  //   id: 5,
+  //   projectName: "TodoList",
+  //   descript: "LocalStorage를 활용하여 오늘의 할일과 기본 할일 기능을 제공합니다.",
+  //   image: projectImg05,
+  //   demoLink: "https://spotbengals.github.io/TodoList/",
+  //   githubLink: "https://github.com/SPOTBENGALS/TodoList",
+  //   keyword: ["React", "Babel", "Webpack", "Typescript"],
+  // },
 ];
